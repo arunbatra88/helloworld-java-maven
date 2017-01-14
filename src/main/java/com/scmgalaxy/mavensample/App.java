79e07f526a60a3dbce716b294032bb5f4ed1d837
arunbatra88 <arun.silverblaze@gmail.com>
2017-01-14 14:10:22 +0530

    Added Space

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!1235Test1" );
-        System.out.println( "Hello World!1235Test2" );
+        System.out.println( "Hello World!1235Test2 " );
     }
 }
